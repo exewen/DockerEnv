3d281cffb8da549f26fc766b010330eb23a14f41
exewen <240175200@qq.com>
2020-08-27 16:25:48 +0800

    新增扩展

---
+++ load_img.bat
@@ -0,0 +1,8 @@
+docker load -i ./images/rabbitmq.3.8.3-management.tar
+docker load -i ./images/redis.3.2.12.tar
+docker load -i ./images/mysql.5.7.tar
+docker load -i ./images/php.7.0.13-fpm.tar
+docker load -i ./images/nginx.1.18.0.tar
+docker load -i ./images/dockerenv_php.tar
+docker load -i ./images/dockerenv_nginx.tar
+pause
