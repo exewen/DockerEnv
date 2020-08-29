4f0988241d2b901c8901eca4e0570cfeca0ea4f6
exewen <240175200@qq.com>
2020-08-29 21:00:51 +0800

    更新

--- mkdir.bat
+++ mkdir.bat
@@ -1,15 +1,17 @@
-mkdir tmp
-mkdir log\mysql
-mkdir log\nginx\access
-mkdir log\nginx\error
-mkdir log\php
-mkdir log\php-fpm
-mkdir log\redis
-mkdir log\rabbitmq
-mkdir mysql
-mkdir project
-mkdir redis\data
-mkdir images
-mkdir rabbitmq\mnesia
-mkdir redis
+if not exist tmp mkdir tmp
+if not exist log\mysql mkdir log\mysql
+if not exist log\nginx\access mkdir log\nginx\access
+if not exist log\nginx\error mkdir log\nginx\error
+if not exist log\php mkdir log\php
+if not exist log\php-fpm mkdir log\php-fpm
+if not exist log\redis mkdir log\redis
+if not exist log\rabbitmq mkdir log\rabbitmq
+if not exist mysql mkdir mysql
+if not exist project mkdir project
+if not exist redis\data mkdir redis\data
+if not exist images mkdir images
+if not exist rabbitmq\mnesia mkdir rabbitmq\mnesia
+if not exist redis mkdir redis
+if not exist 2 mkdir 22
+echo "done"
 pause
