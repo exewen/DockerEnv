f7b062207de8ff13704c5690826e92d67fbd8b04
exewen <240175200@qq.com>
2020-08-29 20:54:26 +0800

    更新

--- mkdir.bat
+++ mkdir.bat
@@ -1,13 +1,15 @@
 mkdir tmp
-mkdir log/mysql
-mkdir log/nginx
-mkdir log/php
-mkdir log/php-fpm
-mkdir log/redis
-mkdir log/rabbitmq
+mkdir log\mysql
+mkdir log\nginx\access
+mkdir log\nginx\error
+mkdir log\php
+mkdir log\php-fpm
+mkdir log\redis
+mkdir log\rabbitmq
 mkdir mysql
 mkdir project
-mkdir redis/data
+mkdir redis\data
 mkdir images
-mkdir rabbitmq/mnesia
+mkdir rabbitmq\mnesia
 mkdir redis
+pause
