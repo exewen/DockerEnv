2adafe60ae8f508d87bbcf763be12f217e851a55
exewen <240175200@qq.com>
2020-09-18 16:00:04 +0800

    更新dns

--- del_img.bat
+++ del_img.bat
@@ -1,2 +1,8 @@
-docker rmi -f xxx
+docker rmi -f 6e5e165e4a0c
+docker rmi -f 2fd7e53ef130
+docker rmi -f 2ad6b739fe1c
+docker rmi -f 718a6da099d8
+docker rmi -f 867da7fcdf92
+docker rmi -f 87856cc39862
+docker rmi -f a4ecb424acb5
 pause
